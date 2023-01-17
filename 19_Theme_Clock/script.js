296d59b6cc3c2c8e68f434e4f4d4674ba1c7615c
asd234 <anujdube0123@gmail.com>
2023-01-17 22:02:17 +0530

    clock

--- 19_Theme_Clock/script.js
+++ 19_Theme_Clock/script.js
@@ -58,55 +58,29 @@ function updateTime() {
   const month = datetime.getMonth();
   const year = datetime.getFullYear();
 
-  const secondsDegrees = (seconds / 60) * 360 + 90;
-  console.log(seconds, secondsDegrees);
-  if (secondsDegrees == 90) {
-    secondEle.classList.add('no-transition');
-  } else {
-    secondEle.classList.remove('no-transition');
-  }
-  secondEle.style.transform = `translate(-50%, -100%) rotate(${secondsDegrees}deg)`;
+  hourEle.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hoursIn12format,
+    0,
+    12,
+    0,
+    360
+  )}deg)`;
 
-  const minutesDegrees = (minutes / 60) * 360 + 0;
-  if (minutesDegrees == 90) {
-    minuteEle.classList.add('no-transition');
-  } else {
-    minuteEle.classList.remove('no-transition');
-  }
-  minuteEle.style.transform = `translate(-50%, -100%) rotate(${minutesDegrees}deg)`;
+  minuteEle.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    60,
+    0,
+    360
+  )}deg)`;
 
-  const hoursDegrees = (hoursIn12format / 60) * 360 + 240;
-
-  if (hoursDegrees == 90) {
-    hourEle.classList.add('no-transition');
-  } else {
-    hourEle.classList.remove('no-transition');
-  }
-  hourEle.style.transform = `translate(-50%, -100%) rotate(${hoursDegrees}deg)`;
-
-  //   hourEle.style.transform = `translate(-50%, -100%) rotate(${scale(
-  //     hoursIn12format,
-  //     0,
-  //     12,
-  //     0,
-  //     360
-  //   )}deg)`;
-
-  //   minuteEle.style.transform = `translate(-50%, -100%) rotate(${scale(
-  //     minutes,
-  //     0,
-  //     60,
-  //     0,
-  //     360
-  //   )}deg)`;
-
-  //   secondEle.style.transform = `translate(-50%, -100%) rotate(${scale(
-  //     seconds,
-  //     0,
-  //     60,
-  //     0,
-  //     360
-  //   )}deg)`;
+  secondEle.style.transform = `translate(-50%, -100%) rotate(${scale(
+    seconds,
+    0,
+    60,
+    0,
+    360
+  )}deg)`;
 
   timeEle.innerHTML = `${hoursIn12format}:${
     minutes < 10 ? `0${minutes}` : minutes
@@ -117,3 +91,5 @@ function updateTime() {
 updateTime();
 
 setInterval(updateTime, 1000);
+
+// In this project clock resetting need to be solved do it
