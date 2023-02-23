425a899632ecd157facff87acd0aa4d0350b9a8c
asd234 <anujdube0123@gmail.com>
2023-02-23 18:15:43 +0530

    fix

--- 50_Catch_Insect_Game/script.js
+++ 50_Catch_Insect_Game/script.js
@@ -54,8 +54,8 @@ function createInsect() {
 function getRandomLocation() {
   const width = window.innerWidth;
   const height = window.innerHeight;
-  const x = Math.random() * (width - 200) + 50;
-  const y = Math.random() * (height - 200) + 50;
+  const x = Math.random() * (width - 200) + 100;
+  const y = Math.random() * (height - 200) + 100;
   return { x, y };
 }
 
